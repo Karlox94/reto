3ab511183491dd3f188c2ef56705809989039294
Carlos Salcedo <caasalce@bancolombia.com.co>
2020-06-02 12:19:44 -0500

    Add my library, casb-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'started';
+  title = 'Biografía de Nikola Tesla';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CasbButtonModule } from 'casb-button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CasbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
